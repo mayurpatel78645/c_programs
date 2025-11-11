77bb36e538bc629e876f78f993796ca447275898
mayur <mayurpatel78645@gmail.com>
2025-11-11 10:56:53 +0530

    add string code

---
+++ class_programs/strings.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+
+  //string length
+  /* char* s = "This is a string.";
+  size_t lenght = strlen(s);
+  printf("%zu", lenght); */
+
+  //char* s = "This is a string.";
+  /* char s[20];
+  printf("Enter a string: ");
+  scanf("%s", &s);
+  int length = strlen(s);
+  printf("%d", length); */
+
+  //reveres string
+  /* char s[30] = "racecar";
+  printf("%s", strrev(s)); */
+
+  //check if palindrome
+  /* char s[30] = "racecar";
+  char rev[30];
+  strcpy(rev, s);
+  strrev(rev);
+  if (strcmp(s, rev) == 0) {
+    printf("%s: is a palindrome.", s);
+  }else{
+    printf("Not a palindrome.");
+  }
+ */
+
+  return 0;
+}
