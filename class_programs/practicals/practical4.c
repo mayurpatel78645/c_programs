9bfe7250c2b2bcc82edbb6ce94e5536a32cdfab3
mayur <mayurpatel78645@gmail.com>
2025-12-18 10:26:08 +0530

    add practical

--- class_programs/practicals/practical4.c
+++ class_programs/practicals/practical4.c
@@ -1,11 +1,33 @@
 #include <stdio.h>
+
+int factorial(int n) {
+  if ( n == 0 || n == 1) {
+    return n;
+  }
+  return n * factorial(n - 1);
+}
+
+int fib(int n) {
+  if (n == 0 || n == 1) {
+    return n;
+  }
+
+  return fib(n - 1) + fib(n - 2);
+}
+
 int main() {
-int n, fact = 1;
+/* int n, fact = 1;
 printf("Enter a number: ");
 scanf("%d", &n);
 for(int i = 1; i <= n; i++) {
 fact *= i;
 }
-printf("Factorial of %d is %d\n", n, fact);
+printf("Factorial of %d is %d\n", n, fact); */
+int n = 8;
+printf("factorial of %d is %d\n", n, factorial(n));
+
+for (int i = 0; i <= n; i++) {
+  printf("%d\n", fib(i));
+}
 return 0;
 }
--- class_programs/practicals/practical7.c
+++ class_programs/practicals/practical7.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+/* #include <stdio.h>
 int main() {
   int n, temp;
   printf("Enter number of elements: ");
@@ -23,5 +23,32 @@ int main() {
     printf("%d ", arr[i]);
   }
   printf("\n");
+  return 0;
+} */
+
+#include <stdio.h>
+
+void bubble_sort(int n, int arr[n]) {
+  for (int i = 0; i < n - 1; i++) {
+    for (int j = 0; j < n - i - 1; j++) {
+      if (arr[j] > arr[j + 1]){
+        int temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+}
+
+int main() {
+
+  int arr[] = {5, 8, 7, 1, 3};
+  int len = sizeof(arr) / sizeof(arr[0]);
+
+  bubble_sort(len, arr);
+
+  for (int i = 0; i < len; i++) {
+    printf("%d\n", arr[i]);
+  }
   return 0;
 }
